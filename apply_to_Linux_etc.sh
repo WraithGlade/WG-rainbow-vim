ebe06a63dda68ffff1190c6fe26dfbf1641e7426
WraithGlade <8360085+WraithGlade@users.noreply.github.com>
2024-09-21 15:55:42 -0400

    Two shell scripts intended for automating synchronizing this repo with the corresponding files where Vim will automatically read them were added.
    I'm currently working on Windows and so I haven't tested the Linux/BSD/Unix shell script yet.

---
+++ apply_to_Linux_etc.sh
@@ -0,0 +1,6 @@
+# This attempts to copy the rainbow scipt from here to 
+# where it will be applied to the user's Vim settings.
+#
+# It is intended for use with Linux/BSD/Unix operating systems.
+
+cp "rainbow.vim" "~\.vim\plugins"
---
+++ apply_to_Windows.bat
@@ -0,0 +1,8 @@
+rem  This attempts to copy the rainbow scipt from here to 
+rem  where it will be applied to the user's Vim settings.
+rem  
+rem  It is intended for use with Windows.
+rem  
+rem  It will warn users about overwrites and ask for confirmation.
+
+xcopy /s /e /i "rainbow.vim" "%userprofile%\vimfiles\plugins"
